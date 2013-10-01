69ba82a60c5046933ca19203d0f97268b52fb882
Julian <j.atienza@har.mrc.ac.uk>
2013-10-01 11:45:17 +0100

    enable tests script

---
+++ scripts/enable_test.sh
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2013 Julian Atienza Herrero <j.atienza at har.mrc.ac.uk>
+#
+# MEDICAL RESEARCH COUNCIL UK MRC
+#
+# Harwell Mammalian Genetics Unit
+#
+# http://www.har.mrc.ac.uk
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+
+for pom in `find .. -name pom.xml`
+	do 	echo $pom
+		sed -i 's/<skipTests>true<\/skipTests>/<skipTests>false<\/skipTests>/' $pom 
+	done
